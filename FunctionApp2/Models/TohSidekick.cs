6b694af89f1cd3ecfbbfda267a6af5a47f098729
Collin Barlow <cwbarlow@3cloudsolutions.com>
2021-04-20 15:12:22 -0500

    Altered the search function from sql filtering to Azure Cognitive Search

---
+++ FunctionApp2/Models/TohSidekick.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToHApi.Models
+{
+    public class TohSidekick
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString(format: "n");
+        public string Name { get; set; }
+        public int Likes { get; set; }
+        public string Hero { get; set; }
+    }
+}
--- FunctionApp2/TohHeroesFunction.cs
+++ FunctionApp2/TohHeroesFunction.cs
@@ -10,21 +10,23 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Newtonsoft.Json;
 using ToHApi.Models;
 using System.Data.SqlClient;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Models;
 
 namespace ApiFunctionAppFrontEnd
 {
     public class TohHeroesFunction
     {
-        string str = Environment.GetEnvironmentVariable("sqldb_connection");
+        readonly string str = Environment.GetEnvironmentVariable("sqldb_connection");
 
         [FunctionName("addHeroFunction")]
-        public async Task<IActionResult> addHero(
+        public async Task<IActionResult> AddHero(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "add")] HttpRequest req)
         {
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             var input = JsonConvert.DeserializeObject<TohHero>(requestBody);
-            var hero = new TohHero() { Name = input.Name, Likes = 1 };
+            var hero = new TohHero() { Name = input.Name, Likes = 1};
             var text = $"INSERT INTO heroes VALUES (@Id, @Name, @Likes)";
 
             using (SqlConnection conn = new SqlConnection(str))
@@ -46,7 +48,7 @@ namespace ApiFunctionAppFrontEnd
         }
 
         [FunctionName("getHeroesFunction")]
-        public IActionResult getHeroes(
+        public IActionResult GetHeroes(
            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "heroes")] HttpRequest req)
         {
             var text = "SELECT * FROM heroes";
@@ -74,7 +76,7 @@ namespace ApiFunctionAppFrontEnd
         }
 
         [FunctionName("getHeroFunction")]
-        public IActionResult getHero(
+        public IActionResult GetHero(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "hero/{id}")] HttpRequest req, string id)
         {
             var text = "Select * FROM heroes WHERE Id = @Id";
@@ -102,7 +104,7 @@ namespace ApiFunctionAppFrontEnd
         }
 
         [FunctionName("updateHeroFunction")]
-        public async Task<IActionResult> updateHero(
+        public async Task<IActionResult> UpdateHero(
             [HttpTrigger(AuthorizationLevel.Function, "put", Route = "hero/{id}")] HttpRequest req, string id)
         {
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
@@ -133,7 +135,7 @@ namespace ApiFunctionAppFrontEnd
         }
 
         [FunctionName("deleteHeroFunction")]
-        public IActionResult deleteHero(
+        public IActionResult DeleteHero(
             [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "hero/{id}")] HttpRequest req, string id)
         {
             var text = $"DELETE FROM heroes WHERE Id = @Id";
@@ -154,10 +156,21 @@ namespace ApiFunctionAppFrontEnd
         }
 
         [FunctionName("searchHeroesFunction")]
-        public IActionResult searchhero(
+        public IActionResult Searchhero(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "search/{name}")] HttpRequest req, string name)
         {
-            var text = $"SELECT * FROM heroes WHERE CHARINDEX(@Name, NAME) > 0";
+            var results = FunctionApp2.Models.Search.Main(name);
+
+            var heroes = new List<TohHero>();
+
+            foreach (SearchResult<TohHero> result in results.GetResults())
+            {
+                heroes.Add(result.Document);
+            }
+
+            return new OkObjectResult(heroes);
+
+            /*var text = $"SELECT * FROM heroes WHERE CHARINDEX(@Name, NAME) > 0";
 
             using (SqlConnection conn = new SqlConnection(str))
             {
@@ -182,8 +195,181 @@ namespace ApiFunctionAppFrontEnd
                     }
                     return new OkObjectResult(Heroes);
                 }
+            }*/
+        }
+
+        // Sidekicks
+
+        [FunctionName("addSidekickFunction")]
+        public async Task<IActionResult> AddSidekick(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "addSidekick")] HttpRequest req)
+        {
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var input = JsonConvert.DeserializeObject<TohSidekick>(requestBody);
+            var sidekick = new TohSidekick() { Name = input.Name, Likes = 1, Hero = input.Hero };
+            var text = $"INSERT INTO sidekicks VALUES (@Id, @Name, @Likes, @Hero)";
+
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                SqlCommand cmd = new SqlCommand(text, conn);
+                conn.Open();
+                List<SqlParameter> param = new List<SqlParameter>()
+                {
+                    new SqlParameter("@Id", System.Data.SqlDbType.VarChar) { Value = sidekick.Id },
+                    new SqlParameter("@Name", System.Data.SqlDbType.VarChar) { Value = sidekick.Name },
+                    new SqlParameter("@Likes", System.Data.SqlDbType.Int) { Value = sidekick.Likes },
+                    new SqlParameter("@Hero", System.Data.SqlDbType.VarChar) { Value = sidekick.Hero }
+                };
+                cmd.Parameters.AddRange(param.ToArray());
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    return new OkObjectResult(sidekick);
+                }
             }
         }
+
+        [FunctionName("getSidekicksFunction")]
+        public IActionResult Getsidekicks(
+           [HttpTrigger(AuthorizationLevel.Function, "get", Route = "sidekicks")] HttpRequest req)
+        {
+            var text = "SELECT * FROM sidekicks";
+
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                SqlCommand cmd = new SqlCommand(text, conn);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    var Sidekicks = new List<TohSidekick>();
+                    while (reader.Read())
+                    {
+                        var sidekick = new TohSidekick
+                        {
+                            Id = (string)reader[0],
+                            Name = (string)reader[1],
+                            Likes = (int)reader[2]
+                        };
+                        Sidekicks.Add(sidekick);
+                    }
+                    return new OkObjectResult(Sidekicks);
+                }
+            }
+        }
+
+        [FunctionName("getHeroSidekicksFunction")]
+        public IActionResult GetHeroSidekicks(
+           [HttpTrigger(AuthorizationLevel.Function, "get", Route = "hero/{id}/sidekicks")] HttpRequest req, string id)
+        {
+            var text = "SELECT * FROM sidekicks WHERE Hero = @Id";
+
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                SqlCommand cmd = new SqlCommand(text, conn);
+                conn.Open();
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "@Id";
+                param.Value = id;
+                cmd.Parameters.Add(param);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    var Sidekicks = new List<TohSidekick>();
+                    while (reader.Read())
+                    {
+                        var sidekick = new TohSidekick
+                        {
+                            Id = (string)reader[0],
+                            Name = (string)reader[1],
+                            Likes = (int)reader[2]
+                        };
+                        Sidekicks.Add(sidekick);
+                    }
+                    return new OkObjectResult(Sidekicks);
+                }
+            }
+        }
+
+        [FunctionName("getSidekickFunction")]
+        public IActionResult GetSidekick(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "sidekick/{id}")] HttpRequest req, string id)
+        {
+            var text = "Select * FROM sidekicks WHERE Id = @Id";
+
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                SqlCommand cmd = new SqlCommand(text, conn);
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "@Id";
+                param.Value = id;
+                cmd.Parameters.Add(param);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    var sidekick = new TohSidekick();
+                    while (reader.Read())
+                    {
+                        sidekick.Id = (string)reader[0];
+                        sidekick.Name = (string)reader[1];
+                        sidekick.Likes = (int)reader[2];
+                        sidekick.Hero = (string)reader[3];
+                    }
+                    return new OkObjectResult(sidekick);
+                }
+            }
+        }
+
+        [FunctionName("deleteSidekickFunction")]
+        public IActionResult DeleteSidekick(
+            [HttpTrigger(AuthorizationLevel.Function, "delete", Route = "sidekick/{id}")] HttpRequest req, string id)
+        {
+            var text = $"DELETE FROM sidekicks WHERE Id = @Id";
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+
+                SqlCommand cmd = new SqlCommand(text, conn);
+                conn.Open();
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "@Id";
+                param.Value = id;
+                cmd.Parameters.Add(param);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    return new OkResult();
+                }
+            }
+        }
+
+        [FunctionName("updateSidekickFunction")]
+        public async Task<IActionResult> UpdateSidekick(
+            [HttpTrigger(AuthorizationLevel.Function, "put", Route = "sidekick/{id}")] HttpRequest req, string id)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var updated = JsonConvert.DeserializeObject<TohHero>(requestBody);
+            var text = $"UPDATE sidekicks SET Name = @Name WHERE Id = @Id";
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+
+                SqlCommand cmd = new SqlCommand(text, conn);
+                conn.Open();
+                List<SqlParameter> param = new List<SqlParameter>()
+                {
+                    new SqlParameter("@Id", System.Data.SqlDbType.VarChar) { Value = id },
+                    new SqlParameter("@Name", System.Data.SqlDbType.VarChar) { Value = updated.Name }
+                };
+                cmd.Parameters.AddRange(param.ToArray());
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    var sidekick = new TohSidekick
+                    {
+                        Id = id,
+                        Name = updated.Name,
+                        Likes = updated.Likes
+                    };
+                    return new OkObjectResult(sidekick);
+                }
+            }
+        }
+
     }
 }
 
